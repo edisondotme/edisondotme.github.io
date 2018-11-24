d65313ba4e80edc189391102d56192715e3ed826
Edison Orellana <me@edison.me>
2018-11-23 21:31:29 -0600

    Added favicon, added jquery, new scheme for repeated html content.

--- static/whoosh.js
+++ static/whoosh.js
@@ -1,6 +1,12 @@
 // whoosh.js
 
-var templatesImport = document.getElementById('templates');
-var templates = templatesImport.import;
-var clone = templates.getElementById('menu');
-document.getElementById('nav').appendChild(clone);
+// this is deprecated code that uses html imports
+// var templatesImport = document.getElementById('templates');
+// var templates = templatesImport.import;
+// var clone = templates.getElementById('menu');
+// document.getElementById('nav').appendChild(clone);
+
+$(document).ready(function(){
+	$("#content").load("link_content.html");
+	;
+});
